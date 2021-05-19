1e67da5da59082f2eb63546e69d7af2ffbe6ab6d
Yvvie <74917027+Yvvie@users.noreply.github.com>
2021-05-19 21:46:34 +0300

    authenticate (#5)

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,21 +4,16 @@ const Session = require('../models/sessionModel')
 
 const authenticate = async (req, res, next) => {
   try {
-    // pasiemam session token per requesto headeri
-    let token = req.headers['twitterauth']
-    // patikrinam ar tokenas yra validus
+    let token = req.headers['userauth']
     let decoded = jwt.verify(token, process.env.JWT_PASSWORD)
-    // patikrinam ar sesija yra dar aktyvi
     let session = await Session.findOne({
       sessionToken: token
     })
     if (!session) throw 'Error'
-    // patikrinam ar egzistuoja toks useris
     let user = await User.findOne({
       _id: decoded.id
     })
     if (!user) throw 'Error'
-    //siunciam userio objekta i kita funkcija
     req.user = user
     req.sessionToken = session
     next()
