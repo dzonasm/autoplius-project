cef3611214d59d38d13e6a792e063cb2f8dde93e
Yvvie <iveta.stugyte@gmail.com>
2021-05-19 19:36:19 +0300

    add user contoler

--- models/userModel.js
+++ models/userModel.js
@@ -1,50 +1,34 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-
 const userSchema = new mongoose.Schema(
 	{
 		email: {
 			type: String,
 			required: true,
-			// unique validacija kad nebutu issaugotas objektas duomenu bazej su tokiu paciu email value
 			unique: true,
 		},
 		password: {
 			type: String,
 			required: true,
 		},
-		createdAt: {
-			type: Date,
-			default: Date.now,
-		},
-		profileImage: {
+		name: {
 			type: String,
+			required: true,
+			unique: true
 		},
-	},
-	{
-		toJSON: {
-			transform(doc, ret) {
-				delete ret.password
-				ret.profileImage = 'http://localhost:3000/' + ret.profileImage
-			},
+		Phone: {
+			type: String,
+			required: true,
+			unique: true
 		},
 	},
 )
 
-// schema pre leidzia paleisti funckija prie konkretu ivyki, siuo atveju pries saugant objekata DB
 userSchema.pre('save', function (next) {
-	//naudojam paprasta funckoja. This yra useris kuris bus saugomas DB
 	let user = this
-	// isModified grazina true arba false jeigu nustatyas parametras yra keiciamas (turi nauja reiksme)
 	if (user.isModified('password')) {
-		// hashinam nauja slaptazodi
 		let hash = bcrypt.hashSync(user.password, 10)
-		// priskiriam hashinta slaptazodi vartotojui
 		user.password = hash
-		// leidziam toliau saugoti useri DB
 		next()
 	} else {
-		// leidziam toliau saugoti useri DB
 		next()
 	}
 })
