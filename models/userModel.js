8fe0264bc474ba827b2c22c7aae3c52adeedab29
Jonas <jonas.marciulevicius@halopowered.com>
2021-05-19 20:54:55 +0300

    fix user models imports

--- models/userModel.js
+++ models/userModel.js
@@ -1,42 +1,43 @@
-const userSchema = new mongoose.Schema(
-	{
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		name: {
-			type: String,
-			required: true,
-			unique: true
-		},
-		Phone: {
-			type: String,
-			required: true,
-			unique: true
-		},
-		createdAt: {
-			type: Date,
-			default: Date.now
-		}
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
 	},
-)
+	password: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	Phone: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
 
 userSchema.pre('save', function (next) {
-	let user = this
+	let user = this;
 	if (user.isModified('password')) {
-		let hash = bcrypt.hashSync(user.password, 10)
-		user.password = hash
-		next()
+		let hash = bcrypt.hashSync(user.password, 10);
+		user.password = hash;
+		next();
 	} else {
-		next()
+		next();
 	}
-})
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-module.exports = User
+module.exports = User;
