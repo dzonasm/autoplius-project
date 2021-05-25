196467fddb5c27eaa9f44e9cb0eb785f239772ae
Jonas Marciulevičius <69348657+dzonasm@users.noreply.github.com>
2021-05-25 13:29:00 -0700

    add search car functionality (#11)
    Co-authored-by: Jonas <jonas.marciulevicius@halopowered.com>

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,16 @@
-const router = require('express').Router();
-const multer = require('multer');
+const router = require("express").Router();
+const multer = require("multer");
 
-const carController = require('../controllers/carController');
-const userController = require('../controllers/userController');
-const authenticateMiddleware = require('../middleware/authenticate');
+const carController = require("../controllers/carController");
+const userController = require("../controllers/userController");
+const authenticateMiddleware = require("../middleware/authenticate");
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, 'uploads');
+		cb(null, "uploads");
 	},
 	filename: (req, file, cb) => {
-		cb(null, Date.now() + '-' + file.originalname);
+		cb(null, Date.now() + "-" + file.originalname);
 	},
 });
 
@@ -19,23 +19,27 @@ const upload = multer({
 });
 
 //cars
-router.route('/cars').get(carController.getAllCars);
-router.route('/cars/mycars').get(authenticateMiddleware.authenticate, carController.getMyCarPosts);
-router.route('/cars/mycars').post(authenticateMiddleware.authenticate, upload.single('carImage') ,carController.createPost);
-router.route('/cars/editCarInfo').post(authenticateMiddleware.authenticate, upload.single('updateCarImg'), carController.editCarInfo)
-router.route('/cars/delete').delete(authenticateMiddleware.authenticate, carController.deleteCarPost);
+router.route("/cars").get(carController.getAllCars);
+router.route("/cars/mycars").get(authenticateMiddleware.authenticate, carController.getMyCarPosts);
+router.route("/cars/mycars").post(authenticateMiddleware.authenticate, carController.createPost);
+router
+	.route("/cars/editCarInfo")
+	.post(authenticateMiddleware.authenticate, upload.single("updateCarImg"), carController.editCarInfo);
+router.route("/cars/delete").delete(authenticateMiddleware.authenticate, carController.deleteCarPost);
+router.route("/cars/searchCar").get(carController.searchCars);
 
-
-router.route('/cars/search').get((req, res) => {
-	res.send('serching for cars');
+router.route("/cars/search").get((req, res) => {
+	res.send("serching for cars");
 });
 
-router.route('/user/editUserInfo').post(authenticateMiddleware.authenticate, userController.editUserInfo)
-router.route('/user/updateUserInfo').post(authenticateMiddleware.authenticate, upload.single('avatar'),userController.updateUserInfo);
+router.route("/user/editUserInfo").post(authenticateMiddleware.authenticate, userController.editUserInfo);
+router
+	.route("/user/updateUserInfo")
+	.post(authenticateMiddleware.authenticate, upload.single("avatar"), userController.updateUserInfo);
 
-router.route('/user/signUp').post(userController.signUp);
-router.route('/user/signIn').post(userController.signIn);
-router.route('/user/currentUser').get(authenticateMiddleware.authenticate, userController.currentUser);
-router.route('/user/logOut').post(authenticateMiddleware.authenticate, userController.logOut);
+router.route("/user/signUp").post(userController.signUp);
+router.route("/user/signIn").post(userController.signIn);
+router.route("/user/currentUser").get(authenticateMiddleware.authenticate, userController.currentUser);
+router.route("/user/logOut").post(authenticateMiddleware.authenticate, userController.logOut);
 
 module.exports = router;
